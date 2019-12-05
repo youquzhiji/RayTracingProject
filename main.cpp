f0f2ca5d21de57eaef9f2fa9931be95b2e1f423c
李林溪 <luigili@yeah.net>
2019-12-04 17:49:16 -0700

    quick update in future for triangle

--- main.cpp
+++ main.cpp
@@ -8,19 +8,166 @@
 #include <random>
 #include <algorithm>
 #include <Eigen>
-
 #define MAX_RAY_DEPTH 5 
 using namespace Eigen;
 
 // image background color
 Vector3f bgcolor(1.0f, 1.0f, 1.0f);
 
+std::vector<float> g_meshVertices;
+std::vector<float> g_meshNormals;
+std::vector<unsigned int> g_meshIndices;
+float g_modelViewMatrix[16];
+std::vector<float> cross(std::vector<float> a, std::vector<float> b)
+{
+	std::vector<float> result = std::vector<float>();
+	result.push_back(a[1] * b[2] - b[1] * a[2]);
+	result.push_back(a[2] * b[0] - b[2] * a[0]);
+	result.push_back(a[0] * b[1] - b[0] * a[1]);
+	/*float normalizer = sqrtf(result[0] * result[0] + result[1] * result[1] + result[2] * result[2]);
+	result[0] = (result[0] / normalizer);
+	result[1] = (result[1] / normalizer);
+	result[2] = (result[2] / normalizer);*/
+	return result;
+
+}
+void computeNormals()
+{
+	g_meshNormals.resize(g_meshVertices.size());
+
+	// TASK 1
+	// The code below sets all normals to point in the z-axis, so we get a boring constant gray color
+	// The following should be replaced with your code for normal computation
+	for (int v = 0; v < g_meshIndices.size() / 3; ++v)
+	{
+		int k = v * 3 + 1;
+		std::vector<float> a = std::vector<float>();
+		std::vector<float> b = std::vector<float>();
+		a.push_back(g_meshVertices[3 * g_meshIndices[k - 1]] - g_meshVertices[3 * g_meshIndices[k]]);
+		a.push_back(g_meshVertices[3 * g_meshIndices[k - 1] + 1] - g_meshVertices[3 * g_meshIndices[k] + 1]);
+		a.push_back(g_meshVertices[3 * g_meshIndices[k - 1] + 2] - g_meshVertices[3 * g_meshIndices[k] + 2]);
+
+		b.push_back(g_meshVertices[3 * g_meshIndices[k]] - g_meshVertices[3 * g_meshIndices[1 + k]]);
+		b.push_back(g_meshVertices[3 * g_meshIndices[k] + 1] - g_meshVertices[3 * g_meshIndices[1 + k] + 1]);
+		b.push_back(g_meshVertices[3 * g_meshIndices[k] + 2] - g_meshVertices[3 * g_meshIndices[1 + k] + 2]);
+
+		std::vector<float>normal = cross(a, b);
+
+		g_meshNormals[3 * g_meshIndices[k]] += normal[0];
+		g_meshNormals[3 * g_meshIndices[k] + 1] += normal[1];
+		g_meshNormals[3 * g_meshIndices[k] + 2] += normal[2];
+
+		g_meshNormals[3 * g_meshIndices[k + 1]] += normal[0];
+		g_meshNormals[3 * g_meshIndices[k + 1] + 1] += normal[1];
+		g_meshNormals[3 * g_meshIndices[k + 1] + 2] += normal[2];
+
+		g_meshNormals[3 * g_meshIndices[k - 1]] += normal[0];
+		g_meshNormals[3 * g_meshIndices[k - 1] + 1] += normal[1];
+		g_meshNormals[3 * g_meshIndices[k - 1] + 2] += normal[2];
+
+
+	}
+
+	/*for (int v = 0; v < g_meshNormals.size() / 3 - 1; ++v)
+	{
+
+		g_meshNormals[3 * v] = (g_meshNormals[3 * v]+ g_meshNormals[3 * v+3])/2;
+		g_meshNormals[3 * v+1] = (g_meshNormals[3 * v+1] + g_meshNormals[3 * v +1+3]) / 2;
+		g_meshNormals[3 * v+2] = (g_meshNormals[3 * v+2] + g_meshNormals[3 * v +2+ 3]) / 2;
+
+
+	}
+	*/
+	for (int v = 0; v < g_meshNormals.size() / 3; ++v)
+	{
+		float normalizer = sqrtf(g_meshNormals[v * 3] * g_meshNormals[v * 3] + g_meshNormals[v * 3 + 1] * g_meshNormals[v * 3 + 1] + g_meshNormals[v * 3 + 2] * g_meshNormals[v * 3 + 2]);
+		g_meshNormals[v * 3] = (g_meshNormals[v * 3] / normalizer);
+		g_meshNormals[v * 3 + 1] = (g_meshNormals[v * 3 + 1] / normalizer);
+		g_meshNormals[v * 3 + 2] = (g_meshNormals[v * 3 + 2] / normalizer);
+
+	}
+	std::vector<float> a = std::vector<float>();
+	std::vector<float> b = std::vector<float>();
+	a.push_back(1.0f);
+	a.push_back(2.0f);
+	a.push_back(3.0f);
+
+	b.push_back(4.0f);
+	b.push_back(5.0f);
+	b.push_back(6.0f);
+	std::vector<float>result = cross(a, b);
+	std::cout << "compute cross product" << result[0] << ", " << result[1] << ", " << result[2] << "\n";
+
+}
+
+void loadObj(std::string p_path)
+{
+	std::ifstream nfile;
+	nfile.open(p_path);
+	std::string s;
+
+	while (nfile >> s)
+	{
+		if (s.compare("v") == 0)
+		{
+			float x, y, z;
+			nfile >> x >> y >> z;
+			g_meshVertices.push_back(x);
+			g_meshVertices.push_back(y);
+			g_meshVertices.push_back(z);
+		}
+		else if (s.compare("f") == 0)
+		{
+			std::string sa, sb, sc;
+			unsigned int a, b, c;
+			nfile >> sa >> sb >> sc;
+
+			a = std::stoi(sa);
+			b = std::stoi(sb);
+			c = std::stoi(sc);
+
+			g_meshIndices.push_back(a - 1);
+			g_meshIndices.push_back(b - 1);
+			g_meshIndices.push_back(c - 1);
+		}
+		else
+		{
+			std::getline(nfile, s);
+		}
+	}
+
+	computeNormals();
+
+	std::cout << p_path << " loaded. Vertices: " << g_meshVertices.size() / 3 << " Triangles: " << g_meshIndices.size() / 3 << std::endl;
+}
 // lights in the scene
 std::vector<Vector3f> lightPositions = { Vector3f(  0.0, 60, 60)
                                        , Vector3f(-60.0, 60, 60)
                                        , Vector3f( 60.0, 60, 60) };
+class Shape 
+{
+public:
+	virtual Vector3f getsurfaceColor()const=0;
+	virtual float getreflect()const=0;
+	virtual float getrefract()const=0;
+	virtual bool intersect(const Vector3f &rayOrigin, const Vector3f &rayDirection, float &t0, float &t1) const=0;
+	virtual Vector3f getNormal(const Vector3f &RayOrigin)const=0;
+};
+class Triangle:Shape
+{
+	Vector3f vert1;
+	Vector3f vert2;
+	Vector3f vert3;
+	Vector3f surfaceColor;
+	bool intersect(const Vector3f &rayOrigin, const Vector3f &rayDirection, float &t0, float &t1) const 
+	{
+		return true;
+	}
+	Vector3f getNormal() {}
+};
 
-class Sphere
+
+class Sphere:public Shape
 {
 public:
 	Vector3f center;  // position of the sphere
@@ -29,7 +176,7 @@ public:
 	float reflect;
 	float refract;
 	
-	Sphere(
+	explicit Sphere(
 		const Vector3f &c,
 		const float &r,
 		const Vector3f &sc,float reflec, float refrac) :
@@ -38,7 +185,19 @@ public:
 	}
 
     // line vs. sphere intersection (note: this is slightly different from ray vs. sphere intersection!)
-	bool intersect(const Vector3f &rayOrigin, const Vector3f &rayDirection, float &t0, float &t1) const
+	Vector3f getsurfaceColor()const override 
+	{
+		return surfaceColor;
+	}
+	 float getreflect()const override 
+	 {
+		 return reflect;
+	 }
+	 float getrefract()const override 
+	 {
+		 return refract;
+	 }
+	bool intersect(const Vector3f &rayOrigin, const Vector3f &rayDirection, float &t0, float &t1) const override
 	{
 		Vector3f l = center - rayOrigin;
 		float tca = l.dot(rayDirection);
@@ -50,6 +209,10 @@ public:
 		t1 = tca + thc;
 
 		return true;
+	}
+	Vector3f getNormal(const Vector3f &RayOrigin) const override
+	{
+		return (RayOrigin - center).normalized();
 	}
 };
 Vector3f mul(Vector3f a, Vector3f b) 
@@ -95,7 +258,7 @@ float mix(const float &a, const float &b, const float &mix)
 Vector3f trace(
 	const Vector3f &rayOrigin,
 	const Vector3f &rayDirection,
-	const std::vector<Sphere> &spheres, const int &depth)
+	const std::vector<Shape*> &spheres, const int &depth)
 {
 	float bias = 1e-4;
 	Vector3f pixelColor = Vector3f::Zero();
@@ -106,7 +269,7 @@ Vector3f trace(
 	int size = spheres.size();
 	for (int k = 0; k < size; k++)
 	{
-		if (spheres[k].intersect(rayOrigin, rayDirection, t0, t1) == true)
+		if ((*spheres[k]).intersect(rayOrigin, rayDirection, t0, t1) == true)
 		{
 			if (fmin(t0, t1) < t&&fmin(t0, t1)>0)
 			{
@@ -119,12 +282,12 @@ Vector3f trace(
 		
 	}
 	//refelction/refraction
-	if (index != -1 && (spheres[index].reflect > 0 || spheres[index].refract > 0) && depth < MAX_RAY_DEPTH) 
+	if (index != -1 && ((*spheres[index]).getreflect() > 0 || (*spheres[index]).getrefract() > 0) && depth < MAX_RAY_DEPTH) 
 	{
 		pixelColor = { 0,0,0 };
 		Vector3f ShadowOrigin = rayOrigin + t * rayDirection;
 		Vector3f ShadowDirection;
-		Vector3f normal = (ShadowOrigin - spheres[index].center).normalized();
+		Vector3f normal = (*spheres[index]).getNormal(ShadowOrigin);
 		Vector3f reflectDirection = (rayDirection - normal * 2 * rayDirection.dot(normal)).normalized();
 		Vector3f reflection = trace(ShadowOrigin + normal * bias, reflectDirection, spheres, depth + 1);
 		Vector3f refraction = { 0,0,0 };
@@ -136,7 +299,7 @@ Vector3f trace(
 			ShadowDirection = (lightPositions[s] - ShadowOrigin).normalized();
 			for (int k = 0; k < size; k++)
 			{
-				if (spheres[k].intersect(ShadowOrigin, ShadowDirection, t0, t1) == true)
+				if ((*spheres[k]).intersect(ShadowOrigin, ShadowDirection, t0, t1) == true)
 				{
 					block = 1;
 					break;
@@ -145,7 +308,7 @@ Vector3f trace(
 			if (block == 0) {
 
 				pixelColor = pixelColor + phong(ShadowDirection, normal
-					, (rayOrigin - ShadowOrigin).normalized(), spheres[index].surfaceColor, { 1,1,1 }, 1, 3, 100);
+					, (rayOrigin - ShadowOrigin).normalized(), (*spheres[index]).getsurfaceColor(), { 1,1,1 }, 1, 3, 100);
 
 			}
 
@@ -158,7 +321,7 @@ Vector3f trace(
 		
 		float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
 		
-		if (spheres[index].refract==1) {
+		if ((*spheres[index]).getrefract()==1) {
 			pixelColor = { 0,0,0 };
 			float ior = 1.52, eta = (inside) ? ior : 1 / ior;
 			float cosi = ShadowOrigin.dot(rayDirection);
@@ -169,7 +332,7 @@ Vector3f trace(
 		
 		pixelColor += mul((
 			reflection * fresneleffect +
-			refraction * (1 - fresneleffect) * spheres[index].refract) , spheres[index].surfaceColor);
+			refraction * (1 - fresneleffect) * (*spheres[index]).getrefract()) , (*spheres[index]).getsurfaceColor());
 		
 		return pixelColor;
 	}
@@ -178,7 +341,7 @@ Vector3f trace(
 		pixelColor = { 0,0,0 };
 		Vector3f ShadowOrigin = rayOrigin + t * rayDirection;
 		Vector3f ShadowDirection;
-		Vector3f normal = (ShadowOrigin - spheres[index].center).normalized();
+		Vector3f normal = (*spheres[index]).getNormal(ShadowOrigin);
 		int block = 0;
 		std::vector<int> blocksphere;
 		for (int s = 0; s < 3; s++) 
@@ -187,7 +350,7 @@ Vector3f trace(
 			ShadowDirection = (lightPositions[s] - ShadowOrigin).normalized();
 			for (int k = 0; k < size; k++)
 			{
-				if (spheres[k].intersect(ShadowOrigin, ShadowDirection, t0, t1) == true)
+				if ((*spheres[k]).intersect(ShadowOrigin, ShadowDirection, t0, t1) == true)
 				{
 					block = 1;
 					blocksphere.push_back(k);
@@ -197,11 +360,11 @@ Vector3f trace(
 			if (block == 0) {
 				
 				pixelColor = pixelColor + phong(ShadowDirection, normal
-					, (rayOrigin - ShadowOrigin).normalized(), spheres[index].surfaceColor, {1,1,1}, 1,3,100);
+					, (rayOrigin - ShadowOrigin).normalized(), (*spheres[index]).getsurfaceColor(), {1,1,1}, 1,3,100);
 				
 			}
 			
-			if(block != 0 && blocksphere.size()==1&& spheres[blocksphere[0]].refract==1)
+			if(block != 0 && blocksphere.size()==1&& (*spheres[blocksphere[0]]).getrefract()==1)
 			{
 				float facingratio = -rayDirection.dot(normal);
 				// change the mix value to tweak the effect
@@ -216,7 +379,7 @@ Vector3f trace(
 				float k = 1 - eta * eta * (1 - cosi * cosi);
 				Vector3f refractionDirection = (rayDirection * eta + normal * (eta *  cosi - sqrt(k))).normalized();
 				refraction = trace(ShadowOrigin - normal * bias, refractionDirection, spheres, depth + 1);
-				pixelColor += mul((refraction * (1 - fresneleffect) * spheres[index].refract), spheres[index].surfaceColor);
+				pixelColor += mul((refraction * (1 - fresneleffect) * (*spheres[index]).getrefract()), (*spheres[index]).getsurfaceColor());
 			}
 			
 
@@ -231,7 +394,7 @@ Vector3f trace(
 	return pixelColor;
 }
 
-void render(const std::vector<Sphere> &spheres)
+void render(const std::vector<Shape*> &spheres)
 {
   unsigned width = 640;
   unsigned height = 480;
@@ -277,15 +440,16 @@ void render(const std::vector<Sphere> &spheres)
 
 int main(int argc, char **argv)
 {
-	std::vector<Sphere> spheres;
+	std::vector<Shape*> spheres;
 	// position, radius, surface color, reflection:value refraction: t/f 
 	//background
-	spheres.push_back(Sphere(Vector3f(0.0, -10004, -20), 10000, Vector3f(0.50, 0.50, 0.50),0,0));
+	spheres.push_back(new Sphere(Vector3f(0.0, -10004, -20), 10000, Vector3f(0.50, 0.50, 0.50),0,0));
 	//actual shperes
-	spheres.push_back(Sphere(Vector3f(0.0, 0, -20), 4, Vector3f(1.00, 0.32, 0.36),0.5,0));//red
-	spheres.push_back(Sphere(Vector3f(5.0, -1, -15), 2, Vector3f(0.90, 0.76, 0.46),0.5,1));//yellow
-	spheres.push_back(Sphere(Vector3f(5.0, 0, -25), 3, Vector3f(0.65, 0.77, 0.97),0.5,0));//blue
-	spheres.push_back(Sphere(Vector3f(-5.5, 0, -13), 3, Vector3f(0.90, 0.90, 0.90),0,0));//white
+	spheres.push_back(new Sphere(Vector3f(0.0, 0, -20), 4, Vector3f(1.00, 0.32, 0.36),0.5,0));//red
+	spheres.push_back(new Sphere(Vector3f(5.0, -1, -15), 2, Vector3f(0.90, 0.76, 0.46),0.5,1));//yellow
+	spheres.push_back(new Sphere(Vector3f(5.0, 0, -25), 3, Vector3f(0.65, 0.77, 0.97),0.5,0));//blue
+	spheres.push_back(new Sphere(Vector3f(-5.5, 0, -13), 3, Vector3f(0.90, 0.90, 0.90),0,0));//white
+	loadObj("teapot.obj");
 
 	render(spheres);
 
